fecbae0e6d639be00e96c11cbe80df88709343f7
SigmaX <ericsiggyscott@gmail.com>
2020-11-13 18:17:59 -0500

    Fix an edge case with stochastic_equal() and add a test to cover it.

--- leap_ec/statistical_helpers.py
+++ leap_ec/statistical_helpers.py
@@ -15,6 +15,28 @@ def collect_distribution(function, samples: int):
     return outputs
 
 
+def _normalize_dicts(dict1, dict2):
+    """Convert two dicts to lists that are aligned to each other."""
+
+    def add_keys_from(dist1, dist2):
+        """If dist1 contains a key that dist2 doesn't, add it to dict2."""
+        for k in dist1.keys():
+            if k not in dist2:
+                dist2[k] = 0
+
+    def values_sorted_by_key(dist):
+        """Get the values of dist, sorted by the keys."""
+        return [dist[k] for k in sorted(dist.keys())]
+    
+    add_keys_from(dict1, dict2)
+    add_keys_from(dict2, dict1)
+
+    n_dict1 = values_sorted_by_key(dict1)
+    n_dict2 = values_sorted_by_key(dict2)
+
+    return n_dict1, n_dict2
+
+
 def stochastic_chisquare(expected_distribution, distribution):
     """Use a $\chi^2$ distribution to compute a p-value for the probability of
     rejecting the hypothesis that the given distribution matches the expected
@@ -29,21 +51,7 @@ def stochastic_chisquare(expected_distribution, distribution):
     """
     assert(sum(expected_distribution.values()) == sum(distribution.values())), f"The distributions have {sum(expected_distribution.values())} and {sum(distribution.values())} samples, respectively, but must be equal."
     
-    def add_keys_from(dist1, dist2):
-        """If dist1 contains a key that dist2 doesn't, add it to dict2."""
-        for k in dist1.keys():
-            if k not in dist2:
-                dist2[k] = 0
-
-    def values_sorted_by_key(dist):
-        """Get the values of dist, sorted by the keys."""
-        return [dist[k] for k in sorted(dist.keys())]
-    
-    add_keys_from(expected_distribution, distribution)
-    add_keys_from(distribution, expected_distribution)
-
-    expected_values = values_sorted_by_key(expected_distribution)
-    values = values_sorted_by_key(distribution)
+    values, expected_values = _normalize_dicts(distribution, expected_distribution)
 
     _, p_value = chisquare(values, expected_values)
     return p_value
@@ -73,6 +81,13 @@ def stochastic_equals(expected_distribution, observed_distribution, p=0.001):
     False
     
     """
+
+    # Handle the special case where each variable has only one possible value
+    values, expected_values = _normalize_dicts(observed_distribution, expected_distribution)
+    if len(values) == 1:
+        return values == expected_values
+
+    # Otherwise, do a chi2 test
     p_value = stochastic_chisquare(expected_distribution, observed_distribution)
     return p_value > p
 
